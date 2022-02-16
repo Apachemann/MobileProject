e2839181aba7f24c54710c89a60511e5241f33ec
Apachemann <shawnkorns@mac.com>
2022-02-16 17:40:21 -0600

    Testing Android Studio Git VCS

---
+++ app/src/main/java/com/example/snapnstudy/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.snapnstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button testBtn;
+    FloatingActionButton addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Grab the objects of the button and floating action button
+        testBtn=(Button)findViewById(R.id.test_edit_button1);
+        addBtn=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+
+        testBtn.setOnClickListener(this);
+        addBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        // Do something in response to floating action button
+        if (view==testBtn) {
+            Intent intent = new Intent(this, EditData.class);
+            startActivity(intent);
+        }else if(view==addBtn) {
+            Intent intent = new Intent(this, CreatePicture.class);
+            startActivity(intent);
+        }
+    }
+}
