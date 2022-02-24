1ee7c8ce56790166a852a1ecec4d8207c1095c3c
Apachemann <shawnkorns@mac.com>
2022-02-24 15:38:40 -0600

    camera imp w/ vid tutorial. part1

--- app/src/main/java/com/example/snapnstudy/MainActivity.java
+++ app/src/main/java/com/example/snapnstudy/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.snapnstudy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public static final int CAMERA_ACTION_CODE = 1;
     Button testBtn;
     FloatingActionButton addBtn;
+    ImageView imagePreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Grab the objects of the button and floating action button
+        // Grab the objects of the button, floating action button, and image view
         testBtn=findViewById(R.id.test_edit_button1);
         addBtn=findViewById(R.id.floatingActionButton);
+        imagePreview=findViewById(R.id.ivPreview);
 
         testBtn.setOnClickListener(this);
         addBtn.setOnClickListener(this);
@@ -34,8 +42,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, EditData.class);
             startActivity(intent);
         }else if(view==addBtn) {
-            Intent intent = new Intent(this, CreatePicture.class);
-            startActivity(intent);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, CAMERA_ACTION_CODE);
+            } else {
+                Toast.makeText(MainActivity.this, "The camera is not working",
+                        Toast.LENGTH_SHORT).show();
+            }
+//            Intent intent = new Intent(this, CreatePicture.class);
+//            startActivity(intent);
+        }
+    }
+    
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null) {
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            imagePreview.setImageBitmap(finalPhoto);
         }
     }
 }
