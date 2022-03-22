8c11d01bf83638698b545d2343b412057257f7af
Apachemann <shawnkorns@mac.com>
2022-03-22 18:57:54 -0500

    OCR coded but error

--- app/src/main/java/com/example/snapnstudy/MainActivity.java
+++ app/src/main/java/com/example/snapnstudy/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -17,17 +18,30 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button testBtn;
     FloatingActionButton addBtn;
     ImageView imagePreview;
+    TextView ocrText;
     ActivityResultLauncher<Intent> activityResultLauncher;
     private static final int REQUEST_CAMERA_CODE = 100;
+
+    // (OCR) Create an instance of TextRecognizer
+    TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         testBtn=findViewById(R.id.test_edit_button1);
         addBtn=findViewById(R.id.floatingActionButton);
         imagePreview=findViewById(R.id.ivPreview);
+
+        // Grab the object of the textview to be used for the resultant OCR text
+        ocrText=findViewById(R.id.finalOCRText);
 
         testBtn.setOnClickListener(this);
         addBtn.setOnClickListener(this);
@@ -55,6 +72,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Bundle bundle = result.getData().getExtras();
                     Bitmap finalPhoto = (Bitmap) bundle.get("data");
                     imagePreview.setImageBitmap(finalPhoto);
+
+                    // (OCR) Prepare the input image
+                    InputImage image = InputImage.fromBitmap(finalPhoto, 0);
+
+                    // (OCR) Process the image
+                    Task<Text> processImage =
+                            recognizer.process(image)
+                                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                        @Override
+                                        public void onSuccess(Text visionText) {
+                                            // Task completed successfully
+                                            // ...
+                                            String imageText = visionText.getText();
+                                            ocrText.setText(imageText);
+                                        }
+                                    })
+                                    .addOnFailureListener(
+                                            new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    // Task failed with an exception
+                                                    // ...
+                                                    Toast.makeText(MainActivity.this, "OCR failed with an exception.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                    // (OCR) Extract text from blocks of recognized text
+                    
                 }
             }
         });
@@ -67,11 +113,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if(intent.resolveActivity(getPackageManager()) != null) {
                     activityResultLauncher.launch(intent);
                 } else {
-                    Toast.makeText(MainActivity.this, "The camera is not working",
+                    Toast.makeText(MainActivity.this, "The camera is not working.",
                             Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void processTxt(Text visionText) {
     }
 
     @Override
