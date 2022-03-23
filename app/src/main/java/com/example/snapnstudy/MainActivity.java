3307edb6ad266efd1d19dd6d4814fe613683888c
michaelcook916 <89547831+michaelcook916@users.noreply.github.com>
2022-03-23 09:52:53 -0500

    textview on home to editable text

--- app/src/main/java/com/example/snapnstudy/MainActivity.java
+++ app/src/main/java/com/example/snapnstudy/MainActivity.java
@@ -1,25 +1,25 @@
 package com.example.snapnstudy;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -136,8 +136,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        super.onActivityResult(requestCode, resultCode, data);
 //        if(requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null) {
 //            Bundle bundle = data.getExtras();
-//            Bitmap finalPhoto = (Bitmap) bundle.get("data");
-//            imagePreview.setImageBitmap(finalPhoto);
-//        }
-//    }
+    //        Bitmap finalPhoto = (Bitmap) bundle.get("data");
+    //          imagePreview.setImageBitmap(finalPhoto);
+    //    }
+   // }
 }
