80b7073836a5d8e1b807f55a63b7ceb3f592199a
Apachemann <shawnkorns@mac.com>
2022-03-24 02:19:47 -0500

    button on fixdata moves back to mainactivity

--- app/src/main/java/com/example/snapnstudy/FixData.java
+++ app/src/main/java/com/example/snapnstudy/FixData.java
@@ -2,22 +2,29 @@ package com.example.snapnstudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class FixData extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class FixData extends AppCompatActivity implements View.OnClickListener{
 
     String ocrTextFinal;
     EditText ocrErrorFix;
+    FloatingActionButton saveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fix_data);
 
-        // Grab the object of the edit text field
+        // Grab the object of the edit text field and floating action button
         ocrErrorFix=findViewById(R.id.editTextMultiLine);
+        saveData=findViewById(R.id.floatingActionButton4);
+
+        saveData.setOnClickListener(this);
 
         // Collect the OCR data passed from MainActivity
         Bundle extras = getIntent().getExtras();
@@ -29,5 +36,12 @@ public class FixData extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    public void onClick(View view) {
+        // Do something in response to floating action button
+        if (view==saveData) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
 }
