321fdd79826121281d26b229b6ed43630d48b8ca
Apachemann <shawnkorns@mac.com>
2022-04-18 18:50:51 -0500

    create fill-in-the-blank activity

---
+++ app/src/main/java/com/example/snapnstudy/FillInTheBlankQuestion.java
@@ -0,0 +1,14 @@
+package com.example.snapnstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillInTheBlankQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_in_the_blank_question);
+    }
+}
