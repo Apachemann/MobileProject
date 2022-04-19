330c268e87d9aec1515585a07893e1f90aeab1b4
Apachemann <shawnkorns@mac.com>
2022-04-18 20:26:17 -0500

    basic fill-in-the-blank outline created

--- app/src/main/java/com/example/snapnstudy/FillInTheBlankQuestion.java
+++ app/src/main/java/com/example/snapnstudy/FillInTheBlankQuestion.java
@@ -3,12 +3,37 @@ package com.example.snapnstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class FillInTheBlankQuestion extends AppCompatActivity {
+
+    // Declare initial variables
+    TextView questionBox;
+    String questionData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_in_the_blank_question);
+
+        // Create a new fill-in-the-blank question when the activity is started
+        createFillInTheBlankQuestion();
+    }
+
+    protected void createFillInTheBlankQuestion() {
+        // Grab the object of the text field
+        questionBox=findViewById(R.id.fillInQuestion);
+
+        // Collect the data passed from the Question activity
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            questionData = extras.getString("ocrGenData");
+            // Set the question text field to display the data
+            questionBox.setText(questionData);
+
+        }
     }
 }
